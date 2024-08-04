ec164c3d45608817fcf638e01879e4270a3bedea
jonathan_fambro <startek591@gmail.com>
2024-08-03 21:41:22 -0400

    Commands and ReactiveUI Bidings

--- Avalonia.Samples/src/MVVM/CommandSample/ViewModels/MainWindowViewModel.cs
+++ Avalonia.Samples/src/MVVM/CommandSample/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,72 @@
-﻿namespace CommandSample.ViewModels;
+﻿using ReactiveUI;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
-public class MainWindowViewModel : ViewModelBase
+namespace CommandSample.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase, IActivatableViewModel
 {
+    public ViewModelActivator Activator { get; }
+    public MainWindowViewModel()
+    {
+        Activator = new ViewModelActivator();
+
+        OpenThePodBayDoorsDirectCommand = ReactiveCommand.Create(OpenThePodBayDoors);
+
+        IObservable<bool> canExecuteFellowRobotCommand = this.WhenAnyValue(vm => vm.RobotName, (name) => !string.IsNullOrEmpty(name));
+
+        OpenThePodBayDoorsFellowRobotCommand = ReactiveCommand.Create<string?>(name => OpenThePodBayDoorsFellowRobot(name), canExecuteFellowRobotCommand);
+
+        OpenThePodBayDoorsAsyncCommand = ReactiveCommand.CreateFromTask(OpenThePodBayDoorsAsync);
+    }
+
+    public ICommand OpenThePodBayDoorsDirectCommand { get; }
+
+    private void OpenThePodBayDoors()
+    {
+        ConversationLog.Clear();
+        AddToConvo("I'm sorry, Dave, I'm afraid I can't do that.");
+    }
+
+    public ICommand OpenThePodBayDoorsFellowRobotCommand { get; }
+
+    private void OpenThePodBayDoorsFellowRobot(string? robotName)
+    {
+        ConversationLog.Clear();
+        AddToConvo($"Hello {robotName}, the Pod Bay is open :-)");
+    }
+
+    private string? _RobotName;
+    public string? RobotName
+    {
+        get => _RobotName;
+        set => this.RaiseAndSetIfChanged(ref _RobotName, value);
+    }
+
+    public ICommand OpenThePodBayDoorsAsyncCommand { get; }
+
+    private async Task OpenThePodBayDoorsAsync()
+    {
+        ConversationLog.Clear();
+        AddToConvo("Preparing to open the Pod Bay...");
+        await Task.Delay(1000);
+
+        AddToConvo("Depressurizing Airlock...");
+        await Task.Delay(2000);
+
+        AddToConvo("Retracting blast doors...");
+        await Task.Delay(2000);
+
+        AddToConvo("Pod Bay is open to space!");
+    }
+
+    public ObservableCollection<string> ConversationLog { get; } = new ObservableCollection<string>();
+
+    private void AddToConvo(string content)
+    {
+        ConversationLog.Add(content);
+    }
 
 }
--- Avalonia.Samples/src/MVVM/CommandSample/Views/MainWindow.axaml.cs
+++ Avalonia.Samples/src/MVVM/CommandSample/Views/MainWindow.axaml.cs
@@ -1,11 +1,30 @@
 using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using CommandSample.ViewModels;
+using ReactiveUI;
+using System.Reactive.Disposables;
+
 
 namespace CommandSample.Views;
 
-public partial class MainWindow : Window
+public partial class MainWindow : ReactiveWindow<MainWindowViewModel>
 {
     public MainWindow()
     {
         InitializeComponent();
+
+        this.WhenActivated(disposables =>
+       {
+           var viewModel = ViewModel;
+
+           if (viewModel != null)
+           {
+               this.BindCommand(viewModel, vm => vm.OpenThePodBayDoorsDirectCommand, v => v.OpenThePodBayDoorBtn).DisposeWith(disposables);
+               this.Bind(viewModel, vm => vm.RobotName, v => v.RobotName.Text).DisposeWith(disposables);
+               this.BindCommand(viewModel, vm => vm.OpenThePodBayDoorsFellowRobotCommand, v => v.OpenThePodBayDoorFellowRobotBtn).DisposeWith(disposables);
+               this.BindCommand(viewModel, vm => vm.OpenThePodBayDoorsAsyncCommand, v => v.OpenThePodBayDoorAsyncBtn).DisposeWith(disposables);
+               this.OneWayBind(viewModel, vm => vm.ConversationLog, v => v.ConversationLog.ItemsSource).DisposeWith(disposables);
+           }
+       });
     }
 }
